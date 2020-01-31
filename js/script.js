c76d0a3ac471bbf1f30e135d758e0d27a4bb0c1b
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-31 15:59:45 +0100

    strutturata chiamata Ajax in js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 
+$.ajax(
+  {
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function(){
+
+    },
+    error: function(richiesta, stato, errore){
+      alert('Si è verificato un error' + errore)
+    }
+  }
+);
 });
